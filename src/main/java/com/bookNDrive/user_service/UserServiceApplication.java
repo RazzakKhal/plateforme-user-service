7c38067e50aa117762f1dce62da48bd1e000a829
razzak <khalfallah.razzak@gmail.com>
2025-02-22 01:42:12 +0100

    add config

--- src/main/java/com/bookNDrive/user_service/UserServiceApplication.java
+++ src/main/java/com/bookNDrive/user_service/UserServiceApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		)
 )
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
