64e6a682e7d0eba21cca0ca910789e4f70bae86b
razzak <khalfallah.razzak@gmail.com>
2025-05-18 23:34:17 +0200

    first part ok

--- src/main/java/com/bookNDrive/user_service/controllers/UserController.java
+++ src/main/java/com/bookNDrive/user_service/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -34,7 +35,8 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<User> getUser(Authentication authentication){
+    public ResponseEntity<User> getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return ResponseEntity.ok(userService.getUser(authentication));
     }
 
